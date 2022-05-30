5642c95a455fc536d6fcda68752a28349a48f68e
chabazite <ingallsandrew0@gmail.com>
2022-05-30 10:13:36 -0500

    config file

--- import_postgres.py
+++ import_postgres.py
@@ -1,7 +1,12 @@
 import psycopg2
+import configuration.helper as helper
+config = helper.read_config()
+host = config['Database']['host']
+dbase = config['Database']['dbase']
+userName = config['Database']['userName']
+password = config['Database']['password']
 
-
-conn = psycopg2.connect("host=***REMOVED*** dbname=***REMOVED*** user=***REMOVED*** password = ***REMOVED***")
+conn = psycopg2.connect("host=", host, " dbname=",dbase," user=",userName," password =",password)
 cur = conn.cursor()
 
 columns_alarm = ('rack_num','date_time','alarm')
@@ -20,3 +25,6 @@ with open('device_log.csv', 'r') as f:
     next(f)
     cur.copy_from(f, 'device_log', sep=',', columns=columns_device)
 conn.commit()
+
+
+print(config)
