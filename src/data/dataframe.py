0e56ef57a2c48ece004010d152153e9e48e599b2
Andrew Ingalls <aingalls@stowers.org>
2022-07-02 15:01:47 -0500

    sensor data uploaded

--- src/data/dataframe.py
+++ src/data/dataframe.py
@@ -4,7 +4,7 @@ def createDataFrame():
     '''
     df_temp = pd.read_csv(os.path.join(root, filename),
                           parse_dates=[['Date', 'Time']], encoding="ISO-8859-1", on_bad_lines='skip', engine='python')
-    df_temp['rack_num'] = rack_num
+    df_temp['rack_num'] = rack_number
     df_temp.drop(['Row'], axis=1, inplace=True)
     return df_temp
 
